57c4c4349dbbcada9d4a7496422eb6d139d68f48
MaryAngelFv <90918206+MaryAngelFv@users.noreply.github.com>
2022-07-26 17:05:21 -0500

    Configuración inicial del proyecto

--- comics-challenge/Modules/Home/HomeViewController.swift
+++ comics-challenge/Modules/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  comics-challenge
 //
 //  Created by Brayam Mora on 26/07/22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
